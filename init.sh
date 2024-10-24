270fed732058e7ffd3753d5e83033c0678e5bd87
Froggy111 <lijiayi4789@gmail.com>
2024-10-24 13:36:27 +0800

    Added support for custom address

--- init.sh
+++ init.sh
@@ -11,4 +11,4 @@ echo $TAILSCALE_LOGIN_SERVER
 mkdir -p /root/derper/$TAILSCALE_DERP_HOSTNAME
 
 #Start Tailscale derp server
-/root/go/bin/derper --hostname $TAILSCALE_DERP_HOSTNAME --bootstrap-dns-names $TAILSCALE_DERP_HOSTNAME -certmode $TAILSCALE_DERP_CERTMODE -certdir /root/derper/$TAILSCALE_DERP_HOSTNAME --stun --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
+/root/go/bin/derper --hostname $TAILSCALE_DERP_HOSTNAME --bootstrap-dns-names $TAILSCALE_DERP_HOSTNAME -certmode $TAILSCALE_DERP_CERTMODE -certdir /root/derper/$TAILSCALE_DERP_HOSTNAME -a $TAILSCALE_DERP_ADDR --stun --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
