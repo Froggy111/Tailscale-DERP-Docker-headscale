3317a390dab1209918558482d7cab7282fda1f08
Froggy111 <lijiayi4789@gmail.com>
2024-10-24 14:03:42 +0800

    fixed addr

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-echo "START OF init.sh"
-echo $TAILSCALE_LOGIN_SERVER
-
 #Start tailscaled and connect to tailnet
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state >>/dev/stdout &
 /usr/bin/tailscale up --login-server=$TAILSCALE_LOGIN_SERVER --auth-key=$TAILSCALE_AUTH_KEY >>/dev/stdout &
